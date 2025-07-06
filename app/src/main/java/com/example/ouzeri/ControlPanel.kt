f664f2b5375b73374450445aaf9b49742e3689af
KurRinos <theodoros.niarchos.p@gmail.com>
2025-07-06 14:33:05 +0300

    buttons

--- app/src/main/java/com/example/ouzeri/ControlPanel.kt
+++ app/src/main/java/com/example/ouzeri/ControlPanel.kt
@@ -8,23 +8,35 @@ import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
 
-class MainActivity : AppCompatActivity() {
+class ControlPanel : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
-        setContentView(R.layout.activity_main)
+        setContentView(R.layout.control_panel)
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
             val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
             insets
         }
 
-        //button
-        val btnToControlPanel = findViewById<Button>(R.id.toControlPanel)
-        btnToControlPanel.setOnClickListener {
-            val intent = Intent(this, ControlPanel::class.java)
-            startActivity(intent)
+        val btnOrdersIn = findViewById<Button>(R.id.ordersin)
+        btnOrdersIn.setOnClickListener {
+            val intent1 = Intent(this, OrdersIn::class.java)
+            startActivity(intent1)
         }
+
+        val btnToDishesOut = findViewById<Button>(R.id.dishesout)
+        btnToDishesOut.setOnClickListener {
+            val intent2 = Intent(this, DishesOut::class.java)
+            startActivity(intent2)
+        }
+
+        val btnToHistory = findViewById<Button>(R.id.history)
+        btnToHistory.setOnClickListener {
+            val intent3 = Intent(this, History::class.java)
+            startActivity(intent3)
+        }
+
 
 
     }
